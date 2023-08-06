d3e9721d2a778f941b48559daf769d319f61a77f
yktt-nuane <tyinnagoyauniversity@gmail.com>
2023-08-06 21:31:23 +0900

    feat: introduce Mantine

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
-import './globals.css'
+import MantineProvider from '@/app/MantineProvider'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}> <MantineProvider>{children} </MantineProvider></body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import {Button} from '@/components/Button/Button'
+import { Button } from '@/components/Button/Button';
+import { DateInput } from '@mantine/dates';
 
 export default function Home() {
   const handleClick = () => {
@@ -8,6 +9,11 @@ export default function Home() {
   };
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <DateInput
+        valueFormat="YYYY/MM/DD"
+        label="dateが機能するかチェック"
+        placeholder="クリックするとカレンダーが出る"
+      />
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <Button label='test' primary size="large" onClick={handleClick} />
       </div>
